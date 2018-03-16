7147f8ce520487f37a98cae9cb09eb3a54716a46
Jade Manrique <jade.a.manrique@gmail.com>
2018-03-16 16:24:13 -0700

    Added files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,14 +5,28 @@ $(document).ready(function () {
   $(".team").animate({"opacity": "1.0"}, 1000);
   $(".team img").animate({"border-radius": "25px", "width": "500px"}, 1000);
 
-  var textfield = $("#question");
 
-  $( "#question" ).keypress(function(e) {
-  if ((e.keyCode || e.which) == 13) {
-    window.location.assign("http://www.google.com/search?q=" + textfield.val());
-  }
+  $(".box").hover( function () {
+
+    $(this).animate({opacity: 1.0}, 150);
+
+  }, function() {
+
+    $(this).animate({opacity: 0.5}, 150);
+
+  });
+
+  $(".team img").hover( function () {
+
+    $(this).animate({"border-radius": "0px", width: "510px"}, 150);
+
+  }, function() {
+
+    $(this).animate({"border-radius": "25px", width: "500px"}, 150);
+
+  });
 
 
-});
+
 
 });
